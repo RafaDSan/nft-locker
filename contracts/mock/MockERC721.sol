8c7768551cfd9913dfe06eddc494b1b9a6ff5876
RafaDSan <rafaeldss12@gmail.com>
2025-11-27 18:25:49 -0300

    updated solidity version and added study comments

--- contracts/mock/MockERC721.sol
+++ contracts/mock/MockERC721.sol
@@ -1,18 +1,22 @@
 // SPD-License-Identifier: MIT
-pragma solidity 0.8.26
+pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MockERC721 is ERC721 {
+contract MockERC721 is ERC721 { // MockERC721 is a derived contract from ERC721, therefore ERC721 is its parent and Mock is inherting its logic
     uint256 public totalSupply;
 
-    constructor() ERC721("MockERC721-LP", "ERC721-LP") {}
+    constructor() ERC721("Mock Uniswap v3 LP", "UNI-V3-POS") {} // When deployed, the constructor is executed once and not anymore
+    // Settings these values into the contract's permanent storage in the moment of deploy
 
     function mint(address to, uint id) public {
+        _mint(to, id); // For productin environment, an alternative is _safeMint to prevent users mistakes 
+        // _safeMint introduces reentrancy attack which developers should update states before the mint happens due to external calls
         totalSupply++;
-        _mint(to, id);
     }
 
+    // The "virtual" keyword is placed in the "tokenURI" signature function coming frm the "ERC721" and it means we can change (override) it
+    // in derived contracts (child contracts)
     function tokenURI(
         uint256
     ) public view virtual override returns (string memory) {
